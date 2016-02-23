372c4e12a98bad1a058739343c56ac4f43fd167e
zhangfei <zhangfeidev@aliyun.com>
2016-02-23 17:18:12 +0800

    熟悉UILable使用
    熟悉一下UILable的使用

--- UI控件学习之UILable/UI控件学习之UILable/ViewController.swift
+++ UI控件学习之UILable/UI控件学习之UILable/ViewController.swift
@@ -42,6 +42,34 @@ class ViewController: UIViewController {
         lable5.adjustsFontSizeToFitWidth = true
         lable5.minimumScaleFactor = 0.8
         self.view.addSubview(lable5)
+        
+        let lable6 = UILabel(frame: CGRectMake(10,280,400,60))
+        lable6.text = "你好，我叫张飞，英文名是Feliu！来自中国贵州，现在是一名即将毕业的大学生，正在学习iOS开发!"
+        lable6.backgroundColor = .grayColor()
+        lable6.numberOfLines = 0
+        self.view.addSubview(lable6)
+
+        let lable7 = UILabel(frame: CGRectMake(10,350,400,80))
+        lable7.text = "Apple Pay is a mobile payment technology that lets users give you their payment information for real-world goods and services in a way that is both convenient and secure."
+        lable7.backgroundColor = .grayColor()
+        lable7.numberOfLines = 0
+        lable7.lineBreakMode = .ByCharWrapping
+        self.view.addSubview(lable7)
+        
+        let lable8 = UILabel(frame: CGRectMake(10,440,400,40))
+        lable8.text = "你好，我叫张飞，英文名是Feliu！来自中国贵州，正在学习iOS开发！"
+        lable8.backgroundColor = .grayColor()
+        lable8.font = UIFont.boldSystemFontOfSize(20)
+        self.view.addSubview(lable8)
+        
+        let lable9 = UILabel(frame: CGRectMake(10,490,400,40))
+        lable9.text = "Apple Pay is a mobile payment technology！"
+        lable9.backgroundColor = .grayColor()
+        lable9.font = UIFont(name: "Zapfino", size: 15)
+        self.view.addSubview(lable9)
+        
+        let fontArr = UIFont.familyNames()
+        print(fontArr)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
