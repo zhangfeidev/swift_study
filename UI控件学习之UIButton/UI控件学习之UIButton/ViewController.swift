687fa27068a033d250f4ba2d007b1a97d733cbe9
zhangfei <zhangfeidev@aliyun.com>
2016-02-25 15:53:06 +0800

    button使用

--- UI控件学习之UIButton/UI控件学习之UIButton/ViewController.swift
+++ UI控件学习之UIButton/UI控件学习之UIButton/ViewController.swift
@@ -22,11 +22,29 @@ class ViewController: UIViewController {
         
         let button2 = UIButton.init(type: UIButtonType.System)
         button2.frame = CGRectMake(50, 150, 100, 100)
-        button2.setImage(UIImage(named: "iconfont-add"), forState: UIControlState.Normal)
+        button2.setBackgroundImage(UIImage(named: "iconfont-add"), forState: UIControlState.Normal)
+        button2.addTarget(self, action: "clickButton2", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(button2)
+        
+        let button3 = UIButton.init(type: UIButtonType.System)
+        button3.frame = CGRectMake(160, 150, 100, 100)
+        button3.setTitle("加号", forState: UIControlState.Normal)
+        button3.setImage(UIImage(named: "iconfont-add"), forState: UIControlState.Normal)
+        button3.addTarget(self, action: "clickButton3", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button3)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func clickButton2(){
+        print("button2 is clicked!")
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+    
+    func clickButton3(){
+        print("button3 is clicked!")
+        self.view.backgroundColor = UIColor.grayColor()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
